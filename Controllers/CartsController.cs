cb419221f7346a8f46258f485b10f7be15ce8709
Keerthana-Ramesh214 <keerthanakowsalya214@gmail.com>
2022-07-07 12:39:08 +0530

    first commit

---
+++ Controllers/CartsController.cs
@@ -0,0 +1,110 @@
+﻿using EcommerceCartAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EcommerceCartAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartsController : ControllerBase
+    {
+        private readonly ECommerceContext _context;
+        public CartsController(ECommerceContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Cart>> Get(int userId)
+        {
+            List<Cart> carts = _context.Carts.Where(x => x.UserId == userId).ToList();
+            return carts;
+        }
+
+        [HttpPost]
+        public string insert(int Prdid, int userId)
+        {
+            if (Prdid == 0)
+            {
+                return "";
+            }
+            try
+            {
+                var product = _context.Products.Find(Prdid);
+                if (product == null)
+                {
+                    return "";
+                }
+                Cart c = new Cart();
+                c.UserId = userId;
+                c.Price = product.Price;
+                c.ProductId = product.ProductId;
+                c.ProductName = product.ProductName;
+                var update =
+             _context.Carts.Where(x => x.ProductId == product.ProductId && x.UserId == userId)
+              .FirstOrDefault();
+                if (update == null)
+                {
+                    c.Quantity = 1;
+                    c.SubTotal = c.Price * c.Quantity;
+                    _context.Carts.Add(c);
+                    _context.SaveChanges();
+                    return c.ProductName+" added";
+                }
+                else
+                {
+                    c.Quantity = update.Quantity + 1;
+                    c.SubTotal = c.Price * c.Quantity;
+                    var updatequery = _context.Carts.Where(x => x.ProductId == product.ProductId && x.UserId == userId)
+                  .FirstOrDefault();
+                    updatequery.Quantity = c.Quantity;
+                    updatequery.SubTotal = c.SubTotal;
+                    _context.SaveChanges();
+                    return c.ProductName+" already exist so Quantity increased";
+                }
+            }
+            catch (Exception e)
+            {
+                return "invalid";
+            }
+        }
+        //Removing 
+        [HttpPut]
+        public string Remove(int Prdid, int userId)
+        {
+            var cart = _context.Carts.Where(x => x.ProductId == Prdid && x.UserId == userId).FirstOrDefault(); ;
+
+            var update =
+         _context.Carts.Where(x => x.ProductId == Prdid && x.UserId == userId)
+          .FirstOrDefault();
+
+
+            if (update != null && update.Quantity == 1)
+            {
+                _context.Carts.Remove(cart);
+                _context.SaveChangesAsync();
+                return "success";
+            }
+            else
+            {
+                cart.Quantity = update.Quantity - 1;
+                cart.SubTotal = cart.Quantity * cart.Price;
+                var updatequery = _context.Carts.Where(x => x.ProductId == cart.ProductId && x.UserId == userId)
+              .FirstOrDefault();
+                updatequery.Quantity = cart.Quantity;
+                updatequery.SubTotal = cart.SubTotal;
+                _context.SaveChanges();
+                return "success";
+            }
+
+        }
+
+
+
+    }
+}
---
+++ Models/ECommerceContext.cs
@@ -0,0 +1,442 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace EcommerceCartAPI.Models
+{
+    public partial class ECommerceContext : DbContext
+    {
+        public ECommerceContext()
+        {
+        }
+
+        public ECommerceContext(DbContextOptions<ECommerceContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Advertisement> Advertisements { get; set; }
+        public virtual DbSet<Cart> Carts { get; set; }
+        public virtual DbSet<Notification> Notifications { get; set; }
+        public virtual DbSet<Offer> Offers { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderItem> OrderItems { get; set; }
+        public virtual DbSet<PaymentDetail> PaymentDetails { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Question> Questions { get; set; }
+        public virtual DbSet<Rating> Ratings { get; set; }
+        public virtual DbSet<Storage> Storages { get; set; }
+        public virtual DbSet<Supplier> Suppliers { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<UserAddress> UserAddresses { get; set; }
+        public virtual DbSet<UserPayment> UserPayments { get; set; }
+        public virtual DbSet<WishList> WishLists { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.;Database=ECommerce;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Advertisement>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.AdsId)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("AdsID");
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany()
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Advertise__Produ__46E78A0C");
+            });
+
+            modelBuilder.Entity<Cart>(entity =>
+            {
+                entity.ToTable("Cart");
+
+                entity.Property(e => e.OfferId).HasColumnName("OfferID");
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.Carts)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Cart__ProductID__34C8D9D1");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Carts)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Cart__UserID__33D4B598");
+            });
+
+            modelBuilder.Entity<Notification>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Body)
+                    .HasMaxLength(1000)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.Title)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.HasOne(d => d.User)
+                    .WithMany()
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Notificat__UserI__4BAC3F29");
+            });
+
+            modelBuilder.Entity<Offer>(entity =>
+            {
+                entity.Property(e => e.OfferId).HasColumnName("OfferID");
+
+                entity.Property(e => e.CreatedOn).HasColumnType("datetime");
+
+                entity.Property(e => e.DeletedOn).HasColumnType("datetime");
+
+                entity.Property(e => e.ModifiedOn).HasColumnType("datetime");
+
+                entity.Property(e => e.OfferName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.Offers)
+                    .HasForeignKey(d => d.ProductId)
+                    .HasConstraintName("FK__Offers__ProductI__2C3393D0");
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.OrderId).HasColumnName("OrderID");
+
+                entity.Property(e => e.DeliveryDate).HasColumnType("date");
+
+                entity.Property(e => e.OfferId).HasColumnName("OfferID");
+
+                entity.Property(e => e.OrderStatus)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.OrderedOn).HasColumnType("datetime");
+
+                entity.Property(e => e.PaymentId).HasColumnName("PaymentID");
+
+                entity.Property(e => e.PaymentType)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.HasOne(d => d.DeliveryAddressNavigation)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.DeliveryAddress)
+                    .HasConstraintName("FK__Orders__Delivery__398D8EEE");
+
+                entity.HasOne(d => d.Offer)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.OfferId)
+                    .HasConstraintName("FK__Orders__OfferID__38996AB5");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Orders__UserID__37A5467C");
+            });
+
+            modelBuilder.Entity<OrderItem>(entity =>
+            {
+                entity.Property(e => e.OfferId).HasColumnName("OfferID");
+
+                entity.Property(e => e.OrderId).HasColumnName("OrderID");
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.ReturnedOn).HasColumnType("datetime");
+
+                entity.HasOne(d => d.Offer)
+                    .WithMany(p => p.OrderItems)
+                    .HasForeignKey(d => d.OfferId)
+                    .HasConstraintName("FK__OrderItem__Offer__3E52440B");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.OrderItems)
+                    .HasForeignKey(d => d.OrderId)
+                    .HasConstraintName("FK__OrderItem__Order__3C69FB99");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.OrderItems)
+                    .HasForeignKey(d => d.ProductId)
+                    .HasConstraintName("FK__OrderItem__Produ__3D5E1FD2");
+            });
+
+            modelBuilder.Entity<PaymentDetail>(entity =>
+            {
+                entity.HasKey(e => e.PaymentId)
+                    .HasName("PK__PaymentD__9B556A58884FF5A5");
+
+                entity.Property(e => e.PaymentId).HasColumnName("PaymentID");
+
+                entity.Property(e => e.CreatedOn).HasColumnType("datetime");
+
+                entity.Property(e => e.Provider)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Reason).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.Active)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("active");
+
+                entity.Property(e => e.Category)
+                    .HasMaxLength(250)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CreatedOn).HasColumnType("datetime");
+
+                entity.Property(e => e.ModifiedOn).HasColumnType("datetime");
+
+                entity.Property(e => e.ProductName).IsRequired();
+            });
+
+            modelBuilder.Entity<Question>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.Qid)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("QID");
+
+                entity.Property(e => e.Question1).HasColumnName("Question");
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany()
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Questions__Produ__4E88ABD4");
+
+                entity.HasOne(d => d.User)
+                    .WithMany()
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Questions__UserI__4D94879B");
+            });
+
+            modelBuilder.Entity<Rating>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.Rating1).HasColumnName("Rating");
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany()
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Ratings__Product__4222D4EF");
+
+                entity.HasOne(d => d.User)
+                    .WithMany()
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Ratings__UserID__4316F928");
+            });
+
+            modelBuilder.Entity<Storage>(entity =>
+            {
+                entity.ToTable("Storage");
+
+                entity.Property(e => e.StorageId).HasColumnName("StorageID");
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.StorageName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.SupplierId).HasColumnName("SupplierID");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.Storages)
+                    .HasForeignKey(d => d.ProductId)
+                    .HasConstraintName("FK__Storage__Product__29572725");
+
+                entity.HasOne(d => d.Supplier)
+                    .WithMany(p => p.Storages)
+                    .HasForeignKey(d => d.SupplierId)
+                    .HasConstraintName("FK__Storage__Supplie__286302EC");
+            });
+
+            modelBuilder.Entity<Supplier>(entity =>
+            {
+                entity.Property(e => e.SupplierId).HasColumnName("SupplierID");
+
+                entity.Property(e => e.Slocation).HasColumnName("SLocation");
+
+                entity.Property(e => e.SupplierName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.Property(e => e.CreatedOn).HasColumnType("datetime");
+
+                entity.Property(e => e.DeletedOn).HasColumnType("datetime");
+
+                entity.Property(e => e.FirstName).HasMaxLength(50);
+
+                entity.Property(e => e.LastLoggedIn).HasColumnType("datetime");
+
+                entity.Property(e => e.LastName).HasMaxLength(50);
+
+                entity.Property(e => e.MailId)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("MailID");
+
+                entity.Property(e => e.Mobile).HasMaxLength(10);
+
+                entity.Property(e => e.ModifiedOn).HasColumnType("datetime");
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(16)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<UserAddress>(entity =>
+            {
+                entity.Property(e => e.AddressLine1).HasMaxLength(50);
+
+                entity.Property(e => e.AddressLine2).HasMaxLength(50);
+
+                entity.Property(e => e.City).HasMaxLength(50);
+
+                entity.Property(e => e.ContactPerson)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Country).HasMaxLength(50);
+
+                entity.Property(e => e.MailId)
+                    .HasMaxLength(50)
+                    .HasColumnName("MailID");
+
+                entity.Property(e => e.Mobile).HasMaxLength(50);
+
+                entity.Property(e => e.PostalCode).HasMaxLength(50);
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.UserAddresses)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__UserAddre__UserI__30F848ED");
+            });
+
+            modelBuilder.Entity<UserPayment>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Expiry).HasColumnType("datetime");
+
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.PaymentType)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ProviderType)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Reference)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.HasOne(d => d.User)
+                    .WithMany()
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__UserPayme__UserI__44FF419A");
+            });
+
+            modelBuilder.Entity<WishList>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("WishList");
+
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany()
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__WishList__Produc__48CFD27E");
+
+                entity.HasOne(d => d.User)
+                    .WithMany()
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__WishList__UserID__49C3F6B7");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
